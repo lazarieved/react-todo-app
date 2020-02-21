b3f15aff90cc07b71401924fbab90c3c9ce58be1
lazarieved <lazarev223@gmail.com>
2020-02-21 16:00:04 +0200

    push todo-app on react

--- src/components/AppComponent.js
+++ src/components/AppComponent.js
@@ -2,152 +2,86 @@ import React from "react";
 import ContainerComponent from "./ContainerComponent";
 import InputAddComponent from "./InputAddComponent";
 import InputSearchComponent from "./InputSearchComponent";
-import {connect} from 'react-redux'
-import {
-  addToDoInState,
-  deleteToDoState,
-  editToDoItem,
-  addSubItem,
-  deleteSubItemsState,
-  editSubItem,
-} from '../actions/appAction'
 
 class App extends React.Component {
-  state = {
-    searchInputValue: '',
-    isSearchInvisible: false,
-  };
-  checkChangeInput = (e) => {
-    this.setState({searchInputValue: e.target.value})
-  };
-  showSearchComponent = (e) => {
-    this.setState(state => ({
-      isSearchInvisible: !state.isSearchInvisible
-    }));
-  };
-  // addToDoInState = (item) => {
-  //     this.setState(state => ({todos: [...state.todos, item]}));
-  // };
-  // deleteToDoState = (item) => {
-  //     this.setState(state => ({todos: [...state.todos].filter(param => item !== param.id)}))
-  // };
-  // deleteSubItemsState = (id, parentId) => {
-  //     const arr = {
-  //         ...this.state.subItems,
-  //         [parentId]: this.state.subItems[parentId].filter(item => id !== item.id)
-  //     };
-  //     this.setState({subItems: arr});
-  // };
-  // editToDoItem = item => {
-  //     this.setState(state => ({
-  //         todos: state.todos.map(param => param.id === item.id ? {...item} : param)
-  //     }));
-  // };
-  // editSubItem = (item, parentId) => {
-  //     const arr = {
-  //         ...this.state.subItems,
-  //         [parentId]: this.state.subItems[parentId].map(param => {
-  //             return param.id === item.id ? {...item} : param;
-  //         })
-  //     };
-  //     this.setState({subItems: arr});
-  // };
-  // addSubItem = (id, item) => {
-  //     this.setState(state => ({
-  //             subItems: {
-  //                 ...state.subItems,
-  //                 [id]: [
-  //                     ...state.subItems[id] || [],
-  //                     item
-  //                 ]
-  //             }
-  //         }
-  //     ));
-  // };
-  // TODO: деструктурировать props и state
-  render() {
-    console.log(this.props);
-    const {
-      todos,
-      addToDoInState,
-      deleteToDoState,
-      editSubItem,
-      deleteSubItemsState,
-      editToDoItem,
-      addSubItem,
-      subItems
-    } = this.props;
-    return (
-      <div className='main-container-app'>
-        <div className='main-title'>ToDo</div>
-        <InputAddComponent addTodo={addToDoInState} fullSize/>
-        <InputSearchComponent
-          handleChange={this.checkChangeInput}
-          searchInputValue={this.state.searchInputValue}
-          isVisible={this.state.isSearchInvisible}
-          showSearchComponent={this.showSearchComponent}
-        />
-        <ContainerComponent
-          searchInputValue={this.state.searchInputValue}
-          todos={todos}
-          deleteToDo={deleteToDoState}
-          editSubItem={editSubItem}
-          deleteSubItemsState={deleteSubItemsState}
-          editToDo={editToDoItem}
-          addSubItem={addSubItem}
-          subTodos={subItems}
-        />
-      </div>
-    );
-  }
+    state = {
+        todos: [],
+        searchInputValue: '',
+        isSearchInvisible: false,
+        subItems: {},
+    };
+    checkChangeInput = (e) => {
+        this.setState({searchInputValue: e.target.value})
+    };
+    showSearchComponent = (e) => {
+        this.setState(state => ({
+            isSearchInvisible: !state.isSearchInvisible
+        }));
+    };
+    addToDoInState = (item) => {
+        this.setState(state => ({todos: [...state.todos, item]}));
+    };
+    deleteToDoState = (item) => {
+        this.setState(state => ({todos: [...state.todos].filter(param => item !== param.id)}))
+    };
+    deleteSubItemsState = (id, parentId) => {
+        const arr = {
+            ...this.state.subItems,
+            [parentId]: this.state.subItems[parentId].filter(item => id !== item.id)
+        };
+        this.setState({subItems: arr});
+    };
+    editToDoItem = item => {
+        this.setState(state => ({
+            todos: state.todos.map(param => param.id === item.id ? {...item} : param)
+        }));
+    };
+    editSubItem = (item, parentId) => {
+        const arr = {
+            ...this.state.subItems,
+            [parentId]: this.state.subItems[parentId].map(param => {
+                return param.id === item.id ? {...item} : param;
+            })
+        };
+        this.setState({subItems: arr});
+    };
+    addSubItem = (id, item) => {
+        this.setState(state => ({
+                subItems: {
+                    ...state.subItems,
+                    [id]: [
+                        ...state.subItems[id] || [],
+                        item
+                    ]
+                }
+            }
+        ));
+    };
+    // TODO: деструктурировать props и state
+    render() {
+        return (
+            <div className='main-container-app'>
+                <div className='main-title'>ToDo</div>
+                <InputAddComponent addTodo={this.addToDoInState} fullSize />
+                <InputSearchComponent
+                    handleChange={this.checkChangeInput}
+                    searchInputValue={this.state.searchInputValue}
+                    isVisible={this.state.isSearchInvisible}
+                    showSearchComponent={this.showSearchComponent}
+                />
+                <ContainerComponent
+                    searchInputValue={this.state.searchInputValue}
+                    todos={this.state.todos}
+                    deleteToDo={this.deleteToDoState}
+                    editSubItem={this.editSubItem}
+                    deleteSubItemsState={this.deleteSubItemsState}
+                    editToDo={this.editToDoItem}
+                    addSubItem={this.addSubItem}
+                    subTodos={this.state.subItems}
+                />
+            </div>
+        );
+    }
 }
 
-
-const mapStateToProps = store => {
-  console.log(store)
-  const {
-    dataReducer: {
-      todos = [
-        // onEdit: false,
-        // inputValue: '123',
-        // id: null,
-        // checked: false,
-        // isSubInputVisible: false,
-      ],
-      searchInputValue = '',
-      isSearchInvisible = false,
-      subItems = [
-        // onEdit: false,
-        // inputValue: '321',
-        // id: null,
-        // checked: false,
-        // isSubInputVisible: false
-      ]
-    },
-  } = store;
-
-  return {
-    todos,
-    subItems,
-    searchInputValue,
-    isSearchInvisible
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addToDoInState: item => dispatch(addToDoInState(item)),
-    deleteToDoState: item => dispatch(deleteToDoState(item)),
-    editToDoItem: item => dispatch(editToDoItem(item)),
-    addSubItem: (id, item) => dispatch(addSubItem(id, item)),
-    deleteSubItemsState: (id, parentId) => dispatch(deleteSubItemsState(id, parentId)),
-    editSubItem: (item, parentId) => dispatch(editSubItem(item, parentId)),
-  }
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(App)
-
-// export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './components/AppComponent';
-import {Provider} from 'react-redux';
-import {store} from './store/configureStore'
 
 ReactDOM.render(
-    <Provider store={store}>
-      {console.log(store)}
-      <App/>
-    </Provider> ,
+    <App/>,
     document.getElementById('root')
 );
